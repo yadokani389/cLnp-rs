d645a1c3ca9ffe2b9ffb204acf106347d941bfdb
yadokani389 <yadokani389@gmail.com>
2024-10-20 19:06:20 +0900

    feat: use poise

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,38 @@
 use dotenv::dotenv;
-use serenity::prelude::*;
+use poise::serenity_prelude as serenity;
 use std::env;
+
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+
+pub struct Data {}
 
 mod commands;
 
 #[tokio::main]
 async fn main() {
-    dotenv().ok();
+    dotenv().expect("Failed to read .env file");
     let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
-    let intents = GatewayIntents::GUILD_MESSAGES
-        | GatewayIntents::DIRECT_MESSAGES
-        | GatewayIntents::MESSAGE_CONTENT;
+    let options = poise::FrameworkOptions {
+        commands: vec![commands::tic_tac_toe::tic_tac_toe()],
+        ..Default::default()
+    };
+    let framework = poise::Framework::builder()
+        .setup(move |ctx, _ready, framework| {
+            Box::pin(async move {
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data {})
+            })
+        })
+        .options(options)
+        .build();
 
-    let mut client = Client::builder(&token, intents)
-        .event_handler(commands::Handler)
-        .await
-        .expect("Err creating client");
+    let intents =
+        serenity::GatewayIntents::non_privileged() | serenity::GatewayIntents::MESSAGE_CONTENT;
 
-    if let Err(why) = client.start().await {
-        println!("Client error: {why:?}");
-    }
+    let client = serenity::ClientBuilder::new(token, intents)
+        .framework(framework)
+        .await;
+
+    client.unwrap().start().await.unwrap()
 }
